955c4e56b51f87916da42c589d9a50f91062af45
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2025-08-05 14:43:06 +0100

    Create get_rev_deps_metadata.py

---
+++ get_rev_deps_metadata.py
@@ -0,0 +1,34 @@
+import collections
+import json
+import pathlib
+
+import requests
+
+
+def main() -> dict:
+    
+    rev_deps = pathlib.Path('asyncio_reverse_deps.txt').read_text().split('\n')
+  
+    meta_data = collections.defaultdict(dict)
+
+    
+    for project_name in rev_deps:
+        project_name = project_name.rstrip()
+
+        if not project_name:
+            continue
+
+        url = f"https://www.pypi.org/pypi/{project_name}/json"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        # meta_data = response.json()['data']['dist_info']['metadata']
+        meta_data[project_name] = response.json()
+
+    pathlib.Path('asyncio_rev_deps_metadata.json').write_text(json.dumps(meta_data))
+
+if __name__ =='__main__':
+    main()
+  
