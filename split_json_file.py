5e94adb74d70990cd574e34daaa279c926e1f8fe
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2025-08-06 11:05:42 +0100

    Update split_json_file.py

--- split_json_file.py
+++ split_json_file.py
@@ -7,17 +7,18 @@ keys = list(d)
 d_pytest = {}
 for k in keys:
     v = d[k]
-    deps = v.get('info',{}).get('requires_dist',None) or []
+    info = v.get('info',{})
+    deps = info.get('requires_dist',None) or []
     if any('pytest' in dep for dep in deps):
-        d_pytest[k] = v
+        d_pytest[k] = info
         d.pop(k)
-pathlib.Path(f'{s}_pytest.json').write_text(json.dumps(d_pytest))
+pathlib.Path(f'{s}_pytest.json').write_text(json.dumps(d_pytest, indent='\t'))
 
 
 keys = list(d)
 N = len(keys)
 step = 255
 for i in range(0,N,step):
-    d_i = {k: d[k] for k in keys[i:min(N,i+step)]}
-    pathlib.Path(f'{s}_{i}.json').write_text(json.dumps(d_i))
+    d_i = {k: d[k].get('info',{}) for k in keys[i:min(N,i+step)]}
+    pathlib.Path(f'{s}_{i}.json').write_text(json.dumps(d_i, indent='\t'))
 
