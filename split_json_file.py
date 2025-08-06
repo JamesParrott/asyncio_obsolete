72c1d141c3a6c5b7ce2d06f9147aa61b436f7e54
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2025-08-06 10:49:43 +0100

    Extract Pytest deps

--- split_json_file.py
+++ split_json_file.py
@@ -3,8 +3,21 @@ s = 'asyncio_rev_deps_metadata'
 d = json.loads(pathlib.Path(f'{s}.json').read_text())
 
 keys = list(d)
-d1 = {k: d[k] for k in keys[:705]}
-d2 = {k: d[k] for k in keys[705:]}
-pathlib.Path(f'{s}_i.json').write_text(json.dumps(d1))
 
-pathlib.Path(f'{s}_ii.json').write_text(json.dumps(d2))
+d_pytest = {}
+for k in keys:
+    v = d[k]
+    deps = v.get('info',{}).get('requires_dist',None) or []
+    if any('pytest' in dep for dep in deps):
+        d_pytest[k] = v
+        d.pop(k)
+pathlib.Path(f'{s}_pytest.json').write_text(json.dumps(d_pytest))
+
+
+keys = list(d)
+N = len(keys)
+step = 255
+for i in range(0,N,step):
+    d_i = {k: d[k] for k in keys[i:min(N,i+step)]}
+    pathlib.Path(f'{s}_{i}.json').write_text(json.dumps(d_i))
+
